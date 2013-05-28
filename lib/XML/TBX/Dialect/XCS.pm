59c698932b48a1de3458038549e3a31eab7fbd2c
Nathan Glenn <garfieldnate@gmail.com>
2013-05-28 11:27:10 -0700

    Finished tests for XCS

--- lib/XML/TBX/Dialect/XCS.pm
+++ lib/XML/TBX/Dialect/XCS.pm
@@ -11,11 +11,11 @@ use Data::Dumper;
 #
 =head1 SYNOPSIS
 
-	my $xcs = XML::Dialect::XCS->new(file=>'/path/to/file.xcs')
+    my $xcs = XML::Dialect::XCS->new(file=>'/path/to/file.xcs')
 
-	my $languages = $xcs->get_languages();
-	my $ref_objects = $xcs->get_ref_objects();
-	my $data_cats = $xcs->get_data_cats();
+    my $languages = $xcs->get_languages();
+    my $ref_objects = $xcs->get_ref_objects();
+    my $data_cats = $xcs->get_data_cats();
 
 =head1 DESCRIPTION
 
@@ -34,9 +34,9 @@ Creates a new XML::TBX::Dialect::XCS object.
 =cut
 
 sub new {
-	my ($class) = @_;
-	my $self = bless {}, $class;
-	return $self;
+    my ($class) = @_;
+    my $self = bless {}, $class;
+    return $self;
 }
 
 =head2 C<parse>
@@ -49,180 +49,183 @@ of the XCS can then be accessed via C<get_ref_objects>, C<get_languages>, and C<
 =cut
 
 sub parse {
-	my ($self, %args) = @_;
+    my ($self, %args) = @_;
 
-	$self->_init;
-	if(exists $args{file}){
-		unless(-e $args{file}){
-			croak "file does not exist: $args{file}";
-		}
-		$self->{twig}->parsefile( $args{file} );
-	}elsif(exists $args{string}){
-		$self->{twig}->parse( ${$args{string}} );
-	}else{
-		croak 'Need to specify either a file or a string pointer with XCS contents';
-	}
-	$self->{xcs_constraints} = $self->{twig}->{xcs_constraints};
-	return;
+    $self->_init;
+    if(exists $args{file}){
+        unless(-e $args{file}){
+            croak "file does not exist: $args{file}";
+        }
+        $self->{twig}->parsefile( $args{file} );
+    }elsif(exists $args{string}){
+        $self->{twig}->parse( ${$args{string}} );
+    }else{
+        croak 'Need to specify either a file or a string pointer with XCS contents';
+    }
+    $self->{xcs_constraints} = $self->{twig}->{xcs_constraints};
+    return;
 }
 
 sub get_languages {
-	my ($self) = @_;
-	return $self->{xcs_constraints}->{languages};
+    my ($self) = @_;
+    return $self->{xcs_constraints}->{languages};
 }
 
 sub get_ref_objects {
-	my ($self) = @_;
-	return $self->{xcs_constraints}->{refObjects} ;
+    my ($self) = @_;
+    return $self->{xcs_constraints}->{refObjects} ;
 }
 
 sub get_data_cats {
-	my ($self) = @_;
-	return $self->{xcs_constraints}->{datCatSet};
+    my ($self) = @_;
+    return $self->{xcs_constraints}->{datCatSet};
 }
 
 sub _init {
-	my ($self) = @_;
-	$self->{twig}->{xcs_constraints} = {};
-	$self->{twig} = _init_twig();
-	return;
+    my ($self) = @_;
+    $self->{twig}->{xcs_constraints} = {};
+    $self->{twig} = _init_twig();
+    return;
 }
 
 sub _run {
-	my ($self, $file) = @_;
-	$self->parse(file => $file);
-	print Dumper $self->{twig}->{xcs_constraints};
-	return;
+    my ($self, $file) = @_;
+    $self->parse(file => $file);
+    print Dumper $self->{twig}->{xcs_constraints};
+    return;
 }
 
 # these are taken from the core structure DTD
 # the types are listed on pg 12 of TBX_spec_OSCAR.pdf
 # TODO: maybe they should be extracted
 my %default_datatype = (
-	adminNote	=> 'plainText',
-	admin		=> 'noteText',
-	descripNote	=> 'plainText',
-	descrip		=> 'noteText',
-	hi			=> 'plainText',
-	ref			=> 'plainText',
-	#I don't think XCS will ever mess with this one in a complicated way
-	#TODO: maybe change this to be shown as 'termCompList' type
-	#TODO: how will we allow users to subset this?
-	termCompList=> 'auxInfo, (termComp | termCompGrp)+',
-	termNote	=> 'noteText',
-	transacNote	=> 'plainText',
-	transac		=> 'plainText',
-	xref		=> 'plainText',
+    adminNote   => 'plainText',
+    admin       => 'noteText',
+    descripNote => 'plainText',
+    descrip     => 'noteText',
+    hi          => 'plainText',
+    ref         => 'plainText',
+    #I don't think XCS will ever mess with this one in a complicated way
+    #TODO: maybe change this to be shown as 'termCompList' type
+    #TODO: how will we allow users to subset this?
+    termCompList=> 'auxInfo, (termComp | termCompGrp)+',
+    termNote    => 'noteText',
+    transacNote => 'plainText',
+    transac     => 'plainText',
+    xref        => 'plainText',
 );
 
 #return an XML::Twig object which will extract data from an XCS file
 sub _init_twig {
-	return new XML::Twig(
-		pretty_print			=> 'indented',
-		# keep_original_prefix	=> 1, #maybe; this may be bad because the JS code doesn't process namespaces yet
-		output_encoding			=> 'UTF-8',
-		do_not_chain_handlers	=> 1, #can be important when things get complicated
-		keep_spaces				=> 0,
-		TwigHandlers			=> {
-			TBXXCS			=> sub {},
-			title			=> sub {},
-			header			=> sub {},
+    return new XML::Twig(
+        pretty_print            => 'indented',
+        # keep_original_prefix  => 1, #maybe; this may be bad because the JS code doesn't process namespaces yet
+        output_encoding         => 'UTF-8',
+        do_not_chain_handlers   => 1, #can be important when things get complicated
+        keep_spaces             => 0,
+        TwigHandlers            => {
+            TBXXCS          => sub {},
+            title           => sub {},
+            header          => sub {},
 
-			languages		=> \&_languages,
-			langCode		=> sub {},
-			langInfo		=> sub {},
-			langName		=> sub {},
+            languages       => \&_languages,
+            langCode        => sub {},
+            langInfo        => sub {},
+            langName        => sub {},
 
-			refObjectDefSet	=> \&_refObjectDefSet,
-			refObjectDef	=> sub {},
-			refObjectType	=> sub {},
-			itemSpecSet		=> sub {},
-			itemSpec		=> sub {},
+            refObjectDefSet => \&_refObjectDefSet,
+            refObjectDef    => sub {},
+            refObjectType   => sub {},
+            itemSpecSet     => sub {},
+            itemSpec        => sub {},
 
-			adminNoteSpec	=> \&_dataCat,
-			adminSpec		=> \&_dataCat,
-			descripNoteSpec	=> \&_dataCat,
-			descripSpec		=> \&_dataCat,
-			hiSpec			=> \&_dataCat,
-			refSpec			=> \&_dataCat,
-			termCompListSpec=> \&_dataCat,
-			termNoteSpec	=> \&_dataCat,
-			transacNoteSpec	=> \&_dataCat,
-			transacSpec		=> \&_dataCat,
-			xrefSpec		=> \&_dataCat,
-			contents		=> sub {},
-			levels			=> sub {},
-			datCatSet		=> sub {},
+            adminNoteSpec   => \&_dataCat,
+            adminSpec       => \&_dataCat,
+            descripNoteSpec => \&_dataCat,
+            descripSpec     => \&_dataCat,
+            hiSpec          => \&_dataCat,
+            refSpec         => \&_dataCat,
+            termCompListSpec=> \&_dataCat,
+            termNoteSpec    => \&_dataCat,
+            transacNoteSpec => \&_dataCat,
+            transacSpec     => \&_dataCat,
+            xrefSpec        => \&_dataCat,
+            contents        => sub {},
+            levels          => sub {},
+            datCatSet       => sub {},
 
-			'_default_'		=> sub {croak 'unknown tag: ' . $_->tag},
-		},
-	);
+            '_default_'     => sub {croak 'unknown tag: ' . $_->tag},
+        },
+    );
 }
 
 ###HANDLERS
 
 #the languages allowed to be used in the document
 sub _languages {
-	my ($twig, $el) = @_;
-	my %languages;
-	#make list of allowed languages and store it on the twig
-	foreach my $language($el->children('langInfo')){
-		$languages{$language->first_child('langCode')->text} =
-			$language->first_child('langName')->text;
-	}
-	$twig->{xcs_constraints}->{languages} = \%languages;
+    my ($twig, $el) = @_;
+    my %languages;
+    #make list of allowed languages and store it on the twig
+    foreach my $language($el->children('langInfo')){
+        $languages{$language->first_child('langCode')->text} =
+            $language->first_child('langName')->text;
+    }
+    $twig->{xcs_constraints}->{languages} = \%languages;
 }
 
 #the reference objects that can be contained in the <back> tag
 sub _refObjectDefSet {
-	my ($twig, $el) = @_;
-	my %defSet;
-		#make list of allowed reference object types and store it on the twig
-	foreach my $def ($el->children('refObjectDef')){
-		$defSet{$def->first_child('refObjectType')->text} =
-			[
-				map {$_->text}
-					$def->first_child('itemSpecSet')->children('itemSpec')
-			];
-	}
+    my ($twig, $el) = @_;
+    my %defSet;
+        #make list of allowed reference object types and store it on the twig
+    foreach my $def ($el->children('refObjectDef')){
+        $defSet{$def->first_child('refObjectType')->text} =
+            [
+                map {$_->text}
+                    $def->first_child('itemSpecSet')->children('itemSpec')
+            ];
+    }
 
-	$twig->{xcs_constraints}->{refObjects} = \%defSet;
+    $twig->{xcs_constraints}->{refObjects} = \%defSet;
 }
 
 # all children of dataCatset
 sub _dataCat {
-	my ($twig, $el) = @_;
-	(my $type = $el->tag) =~ s/Spec$//;
-	my $data = {};
-	$data->{name} = $el->att('name');
-	$data->{datcatId} = $el->att('datcatId');
-	#If the data-category does not take a picklist,
-	#if its data type is the same as that defined for the meta data element in the core-structure DTD,
-	#if its meta data element does not take a target attribute, and
-	#if it does not apply to term components,
-	#this element will be empty and have no attributes specified.
-	my $contents = $el->first_child('contents');
+    my ($twig, $el) = @_;
+    (my $type = $el->tag) =~ s/Spec$//;
+    my $data = {};
+    $data->{name} = $el->att('name');
+    $data->{datcatId} = $el->att('datcatId');
+    #If the data-category does not take a picklist,
+    #if its data type is the same as that defined for the meta data element in the core-structure DTD,
+    #if its meta data element does not take a target attribute, and
+    #if it does not apply to term components,
+    #this element will be empty and have no attributes specified.
+    my $contents = $el->first_child('contents');
 
-	#use default datatypes where not provided
-	#
-	$data->{datatype} =
-		($contents->att('datatype') || $default_datatype{$type});
+    #use default datatypes where not provided
+    $data->{datatype} =
+        ($contents->att('datatype') || $default_datatype{$type});
 
-	$data->{choices} = [split ' ', $contents->text]
-		if($data->{datatype} && $data->{datatype} eq 'picklist');
+    if($data->{datatype} && $data->{datatype} eq 'picklist'){
+        $data->{choices} = [split ' ', $contents->text];
+    }
+    if ($contents->att('forTermComp')){
+        $data->{forTermComp} = $contents->att('forTermComp') eq 'yes' ? 1 : 0;
+    }
 
-	$data->{forTermComp} = $contents->att('forTermComp') eq 'yes' ? 1 : 0
-		if $contents->att('forTermComp');
+    if ($contents->att('targetType')){
+        $data->{targetType} = $contents->att('targetType');
+    }
 
-	$data->{targetType} = $contents->att('targetType')
-		if $contents->att('targetType');
-
-	#levels can be specified for descrip data categories
-	if( $type eq 'descrip' && (my $levels = $el->first_child('levels') ) ){
-		$data->{levels} = [split ' ', $levels->text];
-	}else{
-		$data->{levels} = [qw(langSet termEntry term)]
-	}
-	#also, check page 10 of the OSCAR PDF for elements that can occur at multiple levels
-	push @{ $twig->{xcs_constraints}->{datCatSet}->{$type} }, $data;
+    #levels can be specified for descrip data categories
+    if($type eq 'descrip'){
+        if(my $levels = $el->first_child('levels')->text){
+            $data->{levels} = [split ' ', $levels];
+        }else{
+            $data->{levels} = [qw(langSet termEntry term)]
+        }
+    }
+    #also, check page 10 of the OSCAR PDF for elements that can occur at multiple levels
+    push @{ $twig->{xcs_constraints}->{datCatSet}->{$type} }, $data;
 }
--- t/01-XCS.t
+++ t/01-XCS.t
@@ -1,8 +1,8 @@
-#make sure that the core structure RNG validates a TBX file
+#Check that XCS.pm creates proper structure from an XCS file
 use strict;
 use warnings;
-use Test::More;
-plan tests => 2;
+use Test::More 0.88;
+plan tests => 6;
 use XML::TBX::Dialect::XCS;
 use Path::Tiny;
 use FindBin qw($Bin);
@@ -17,16 +17,60 @@ my $xcs = XML::TBX::Dialect::XCS->new();
 test_xcs_data('file',$xcs_file);
 test_xcs_data('string',\$xcs_contents);
 
-
-# test_xcs_data($xcs);
-
+#test languages, ref objects, and data categories
 sub test_xcs_data {
-	my ($type, $data) = @_;
-	$xcs->parse($type=>$data);
-	is_deeply(
-		$xcs->get_languages(),
-		{en => 'English', fr => 'French', 'de' => 'German'},
-		"Languages extracted from $type"
-	);
+    my ($type, $data) = @_;
+    $xcs->parse($type=>$data);
+
+    is_deeply(
+        $xcs->get_languages(),
+        {en => 'English', fr => 'French', 'de' => 'German'},
+        "Languages extracted from $type"
+    ) or note explain $xcs->get_languages();
+
+    is_deeply(
+        $xcs->get_ref_objects(),
+        { Foo => ['data'] },
+        "Ref objects extracted from $type"
+    ) or note explain $xcs->get_ref_objects();
+
+    is_deeply(
+        $xcs->get_data_cats(),
+        get_expected_data_cat(),
+        "Data categories extracted from $type"
+    ) or note explain $xcs->get_data_cats();
 }
 
+sub get_expected_data_cat {
+    return
+    {
+      'descrip' =>
+      [
+        {
+          'datatype' => 'noteText',
+          'datcatId' => 'ISO12620A-0503',
+          'levels' => ['term'],
+          'name' => 'context'
+        },
+        {
+          'datatype' => 'noteText',
+          'datcatId' => '',
+          'levels' => ['langSet', 'termEntry', 'term'],
+          'name' => 'descripFoo'
+        }
+      ],
+      'termNote' => [{
+          'choices' => ['animate', 'inanimate', 'otherAnimacy'],
+          'datatype' => 'picklist',
+          'datcatId' => 'ISO12620A-020204',
+          'forTermComp' => 1,
+          'name' => 'animacy'
+        }],
+      'xref' => [{
+          'datatype' => 'plainText',
+          'datcatId' => '',
+          'name' => 'xrefFoo',
+          'targetType' => 'external'
+        }]
+    };
+}
